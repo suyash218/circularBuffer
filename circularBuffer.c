038bcc5ace013829a52daa9eb4aa03c237c9c894
Suyash Varma <suyash.varma@broadcom.com>
2017-12-19 15:29:18 -0500

    draft1

---
+++ circularBuffer.c
@@ -0,0 +1,15 @@
+/* Implement a circular buffer with write and read APIs */
+
+#include <stdio.h>
+#include <stdlib.h> /* for malloc etc. */
+
+#define BUFFER_SIZE 10 /* Bytes */
+
+int main()
+{
+  char *ptrToBuffer;
+  ptrToBuffer = malloc(BUFFER_SIZE);
+
+  memset(ptrToBuffer, 0x0);
+  printf("\n%x\n", )
+}
